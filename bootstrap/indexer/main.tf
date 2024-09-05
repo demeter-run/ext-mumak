3ab690ece67a2fa5f7aa1a706cd5cbe5c981c9f8
gonzalezzfelipe <gonzalezz_felipe@hotmail.com>
2024-09-05 20:56:46 -0300

    Fix configurations

--- bootstrap/indexer/main.tf
+++ bootstrap/indexer/main.tf
@@ -2,8 +2,8 @@ locals {
   block_configmap_name = "oura-config-${var.instance_name}-blocks"
   tx_configmap_name    = "oura-config-${var.instance_name}-txs"
 
-  block_instance_name = "${var.instance_name}_block"
-  tx_instance_name    = "${var.instance_name}_tx"
+  block_instance_name = "${var.instance_name}-block"
+  tx_instance_name    = "${var.instance_name}-tx"
 }
 
 variable "namespace" {
