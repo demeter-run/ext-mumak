65b22c2384084a70826e363d9638384e2cf48504
paulobressan <paulo.bressan@outlook.com>
2024-09-23 17:00:25 -0300

    chore: implemented crdgen json output

--- operator/src/crdgen.rs
+++ operator/src/crdgen.rs
@@ -2,8 +2,17 @@ use kube::CustomResourceExt;
 use operator::controller;
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 && args[1] == "json" {
+        print!(
+            "{}",
+            serde_json::to_string_pretty(&controller::MumakPort::crd()).unwrap()
+        );
+        return;
+    }
+
     print!(
         "{}",
-        serde_yaml::to_string(&controller::MumakPort::crd()).unwrap()
+        serde_json::to_string(&controller::MumakPort::crd()).unwrap()
     )
 }
